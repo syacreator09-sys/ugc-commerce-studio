2fb53370ca4c2e3969c0a72915a251b2345c0e86
syacreator09-sys <syacreator09@gmail.com>
2026-08-14 18:35:29 -0600

    feat: add provider-neutral discovery package

---
+++ src/ugc_commerce/discovery/__init__.py
@@ -0,0 +1,4 @@
+from .base import DiscoveryCandidate, DiscoveryProvider
+from .manual import ManualDiscoveryProvider
+
+__all__ = ["DiscoveryCandidate", "DiscoveryProvider", "ManualDiscoveryProvider"]
